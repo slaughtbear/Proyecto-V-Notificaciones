14ed997aac6dabe42dd9c5989c47de4653e72251
slaughtbear <ivan.aguirre1603@gmail.com>
2025-10-24 19:56:04 -0600

    v1 terminada...

--- app/src/main/java/com/example/proyecto_v/MainActivity.java
+++ app/src/main/java/com/example/proyecto_v/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.proyecto_v;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String CANAL_ID = "Mi canal";
+    static final int NOTIF_ID = 1;
+    NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.0) {
+            NotificationManager channel = new NotificationChannel(CANAL_ID,
+                    "Mis notificaciones", NotificationManager.IMPORTANCE_DEFAULT;
+            channel.setDescription("Descripción de mi canal");
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.setVibrationPattern(new long[]{0,100,300,100});
+            channel.enableVibration(true);
+            notificationManager.createNotificationChannel(channel);
+        }
+        
+        Button wearButton = (Button) findViewById(R.id.boton1);
+        wearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder notificacion = new 
+                        NotificationCompat.Builder(MainActivity.this, CANAL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Titulo WOW!")
+                        .setContentText("Esperemos que funcione la practica");
+                        
+                notificationManager.notify(NOTIF_ID, notificacion.build());
+            }
         });
     }
 }
